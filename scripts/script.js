80d82f0cbf5f2d174fe6565d5c036de914b6aad2
Ryan Kristoffer <rkrmarayag2021@plm.edu.ph>
2024-05-04 22:12:50 +0800

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,59 @@
 const voicesDropdown = document.querySelector("#voices");
 const rateInput = document.querySelector("#rate");
 const pitchInput = document.querySelector("#pitch");
-const text = document.querySelector("#text");
+const textarea = document.querySelector("#text");
 const stopButton = document.querySelector("#stop");
 const speakButton = document.querySelector("#speak");
 
-message = new SpeechSynthesisUtterance(text.value);
+const message = new SpeechSynthesisUtterance(textarea.value);
 let voices = [];
 
-function populateVoices() {
-  voices = speechSynthesis.getVoices();
-  for (let index = 0; index < voices.length; index++) {
-    const option = document.createElement("option");
-    option.setAttribute("value", voices[index].name);
-    option.textContent = voices[index].name;
-    voicesDropdown.appendChild(option);
-  }
+function populateVoices(){
+    voices = speechSynthesis.getVoices();
+
+    for(let index = 0; index < voices.length; index++){
+        const option = document.createElement("option");
+        option.setAttribute("value", voices[index].name);
+        option.textContent = voices[index].name;
+        
+        voicesDropdown.appendChild(option);
+    }
 }
 
-function setVoice() {
-  for (let index = 0; index < voices.length; index++) {
-    if (voices[index].name === voicesDropdown.value) {
-      message.voice = voices[index];
+function setVoice(){
+    for(let index =0; index < voices.length; index++){
+        if(voicesDropdown.value === voices[index].name){
+            message.voice = voices[index];
+            break;
+        }
     }
-  }
 }
 
 function setRate() {
-  message.rate = rateInput.value;
+    message.rate = rateInput.value;
 }
 
-function setPitch() {
-  message.pitchInput = pitchInput.value;
+function setPitch(){
+    message.pitch = pitchInput.value;
 }
 
-function setText() {
-  message.text = text.value;
+function setText(){
+    message.text = textarea.value;
 }
 
-function stopVoice() {
-  speechSynthesis.cancel();
+function stopVoice(){
+    speechSynthesis.cancel()
 }
 
 function speakVoice() {
-  speechSynthesis.speak(message);
+    speechSynthesis.speak(message);
 }
+
 
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
 rateInput.addEventListener("change", setRate);
 pitchInput.addEventListener("change", setPitch);
-text.addEventListener("change", setText);
-stopButton.addEventListener("click", stopVoice);
+textarea.addEventListener("change", setText);
+stopButton.addEventListener("click",stopVoice);
 speakButton.addEventListener("click", speakVoice);
